fe30003e04603b66b45148da05b1cb065b1c52f6
Lucky <lucky123@gmail.com>
2025-02-13 18:41:17 +0530

    font setup

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,20 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import SessionProvider from "@/providers/SessionProvider";
+import { Toaster } from "@/components/ui/sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const fontSans = FontSans({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
-  title: "Real-Chart App",
-  description: "To charting quick as possible without any login ",
+  title: "RealChat",
+  description: "RealChat App",
 };
 
 export default function RootLayout({
@@ -24,11 +22,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <SessionProvider>
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+          <Toaster richColors duration={5000} />
+        </body>
+      </SessionProvider>
     </html>
   );
 }
